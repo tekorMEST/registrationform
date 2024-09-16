d4f99dc0b0c2cb6848b6e9552b1864cf11c778ca
tekorMest <marteymartekor55@gmail.com>
2024-09-16 11:57:15 +0000

    add js

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+//Select form by id
+const registerForm = document.getElementById('register-form'); 
+//Handle form submit event
+registerForm.onsubmit = function() {
+ //Collect username
+ //Collecct email
+ //Collect password
+ //Save user information
+ // Send confirmation email   
+}
